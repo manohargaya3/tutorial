4d2cdac42895b842ac9202a5a8c5bb23762aad3b
manoharg <manoharmtechpsgcol@gmail.com>
2019-12-11 18:42:59 +0530

    Tutorials

---
+++ src/main/java/com/manohar/tutorial/PracticeSession.java
@@ -0,0 +1,32 @@
+package com.manohar.tutorial;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class PracticeSession {
+
+    public static void main(String args[]){
+        String input = "this is what we know about this and this is awesome";
+        Map<String, Integer> outPut = new HashMap<String, Integer>();
+        String[] inputValues = input.split(" ");
+        for(String inputValue : inputValues){
+            if(outPut.containsKey(inputValue)){
+                outPut.put(inputValue, outPut.get(inputValue)+1);
+            }
+            else {
+                outPut.put(inputValue,1);
+            }
+        }
+        System.out.println(outPut);
+        int count = 0;
+        String result = "";
+        for(Map.Entry<String, Integer> outPutValue: outPut.entrySet()){
+            if(count < outPutValue.getValue()){
+                result = outPutValue.getKey();
+                count = outPutValue.getValue();
+            }
+        }
+        System.out.println(result);
+    }
+}
